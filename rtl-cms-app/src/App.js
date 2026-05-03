20730d506144eb2eff6e774419e78c86f88a0c32
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-03 09:05:01 +0330

    Add Product ,Comments,Users,Orders,Offs folder with jsx and css files as components Install react-router-dom for adding Routes in project

--- rtl-cms-app/src/App.js
+++ rtl-cms-app/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar/Sidebar'
 import Header from './Components/Header/Header'
+import { Route,Routes } from 'react-router-dom'
+import Comments from './Components/Comments/Comments'
+import Products from './Components/Products/Products'
+import Users from './Components/Users/Users'
+import Orders from './Components/Orders/Orders'
+import Offs from './Components/Offs/Offs'
+
 export default function App() {
   return (
     <>
+
       <Sidebar/>
       <div className='main'>
           <Header/>
+          <Routes>
+            <Route path='/products' element={<Products/>}></Route>
+            <Route path='/comments' element={<Comments/>}></Route>
+            <Route path='/users' element={<Users/>}></Route>
+            <Route path='/orders' element={<Orders/>}></Route>
+            <Route path='/offs' element={<Offs/>}></Route>
+          </Routes>
       </div>
+
     </>
   )
 }
